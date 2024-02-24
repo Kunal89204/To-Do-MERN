a1b50122473a14ea15c4c843dd9e87a704ff6318
Kunal89204 <Kunalkhandelwal108@gmail.com>
2024-02-24 11:16:27 +0530

    backend api's created

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,47 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+const taskSchema = require("./users");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  res.send("helllo world");
 });
+
+router.get("/tasks", async (req, res) => {
+  try {
+    const data = await taskSchema.find();
+    res.json(data);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.post("/addtask", async (req, res) => {
+  try {
+    const newTask = await taskSchema.create(req.body);
+    res.json(newTask);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.delete("/delete/:id", async (req, res) => {
+  try {
+    const deletedTask = await taskSchema.findByIdAndDelete(req.params.id);
+    res.json(deletedTask);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.put("/update/:id", async (req, res) => {
+  try {
+    const updatedTask = await taskSchema.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updatedTask);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
 
 module.exports = router;
